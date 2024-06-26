b1229120701c866734c53029c694d356745ae4d9
Kii5881 <119861985+Kii5881@users.noreply.github.com>
2024-06-26 21:03:42 +0700

    Add the first few files via upload

---
+++ scripts/main.js
@@ -0,0 +1,42 @@
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/calico cat kitten.jpg") {
+    myImage.setAttribute("src", "images/calico cat kitten2.jpg");
+  } else {
+    myImage.setAttribute("src", "images/calico cat kitten.jpg");
+  }
+};
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+    const myName = prompt("Please enter your name.");
+    localStorage.setItem("name", myName);
+    myHeading.textContent = `calico cat is cute, ${myName}`;
+  }
+
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla is cool, ${storedName}`;
+  }
+
+  myButton.onclick = () => {
+    setUserName();
+  };
+
+  function setUserName() {
+    const myName = prompt("Please enter your name.");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `Mozilla is cool, ${myName}`;
+    }
+  }
+  
+  
